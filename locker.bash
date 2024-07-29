28d80893632f5b98e5a96d9929a5890846b2709a
Alex Elman <chicagoatnite@gmail.com>
2024-07-29 03:50:34 -0500

    Improvements to screen locker

--- locker.bash
+++ locker.bash
@@ -4,4 +4,5 @@ TMPBG=/dev/shm/screen.png
 scrot -o /dev/shm/screen.png
 convert $TMPBG -scale 10% -scale 1000% $TMPBG
 convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
-i3lock -ef -I 15 -i $TMPBG
+i3lock -ef -i $TMPBG
+sleep 2 && xset dpms force off
