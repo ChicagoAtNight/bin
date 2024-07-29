65e67c3941520c7b8566bdfb13ca520a05bb0651
Alex Elman <chicagoatnite@gmail.com>
2024-07-29 00:38:28 -0500

    Scrot command

--- locker.bash
+++ locker.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 ICON=$HOME/Pictures/lock_med.png
 TMPBG=/dev/shm/screen.png
-scrot /dev/shm/screen.png
+scrot -o /dev/shm/screen.png
 convert $TMPBG -scale 10% -scale 1000% $TMPBG
 convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
 i3lock -ef -I 15 -i $TMPBG
